df1b21da2154300250a6c20fff36861a86678429
zjc_chuanchuan <ZSC1104024838>
2019-02-12 17:26:44 +0800

    增加新的测试

--- src/main/java/com/aqs/CountDownLatchTest.java
+++ src/main/java/com/aqs/CountDownLatchTest.java
@@ -14,6 +14,8 @@ public class CountDownLatchTest {
 
     private static final int THREAD_COUNT = 500;
 
+    private static volatile int TOTAL_MONEY ;
+
     public static void main(String[] args) throws Exception{
         ExecutorService executorService = Executors.newFixedThreadPool(300);
         final CountDownLatch downLatch = new CountDownLatch(THREAD_COUNT);
@@ -21,8 +23,8 @@ public class CountDownLatchTest {
             final int threadNum = a;
             executorService.execute(() -> {
                 try {
-                    test(threadNum);
-                } catch (InterruptedException e) {
+                    getCount();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }finally {
                     //减少锁存器的计数,如果当前计数大于零,则它将递减.如果计数达到零，释放所有等待的线程
@@ -34,8 +36,12 @@ public class CountDownLatchTest {
         //等到锁存器计数为零时,才能继续进行下去
         downLatch.await();
         executorService.shutdown();
-        System.out.println("finish");
+        System.out.println("finish" + TOTAL_MONEY);
 
+    }
+
+    private static void getCount() {
+        TOTAL_MONEY = TOTAL_MONEY +1;
     }
 
 
