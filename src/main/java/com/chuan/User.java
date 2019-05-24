07448a1e56286530e18f5b943fda40af72d9f168
jichuan <jichuandada@gmail.com>
2019-05-24 13:34:29 +0800

    测试

---
+++ src/main/java/com/chuan/User.java
@@ -0,0 +1,19 @@
+package com.chuan;
+
+import lombok.Data;
+
+/**
+ * @author jc
+ * @Description:
+ * @date 2019/5/21
+ */
+@Data
+public class User {
+
+    private String name;
+    private Integer age;
+    private String address;
+
+
+
+}
